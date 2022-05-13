7831fb1fae10920a892d6e42e58629e08810d729
prasanta <prasantakakati1994@gmail.com>
2022-05-14 00:08:01 +0530

    temp api

--- app/api/api_v1/endpoints/payment_form/payment.py
+++ app/api/api_v1/endpoints/payment_form/payment.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Query
+from fastapi import APIRouter, Depends, HTTPException, status, Query, Body
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app.db import get_default_database
@@ -7,6 +7,7 @@ from app.models.payments import Payment, PaymentCreate, PaymentCreateResponse
 from app.utils.payment import verify_form_id, verify_payment_id
 from app.crud import payments
 from app.utils import payment as payment_utils
+from app.worker import arq_manager
 
 payment_router = APIRouter()
 
@@ -35,3 +36,31 @@ async def get_payment_status(
         db: AsyncIOMotorDatabase = Depends(get_default_database)):
     await verify_payment_id(db, form_id, payment_id)
     return await payment_utils.get_payment_status(db, payment_id)
+
+
+@payment_router.post('/add_to_queue')
+async def add_to_queue(
+        payment_id: str = Body(...),
+        db: AsyncIOMotorDatabase = Depends(get_default_database)
+):
+    payment = await db.payments.find_one({'payment_id': payment_id})
+    project = await db.projects.find_one(
+        {'id': payment['related_project_id']})
+    payout_queue = await db.payout_queues.find_one(
+        {
+            'owner_id': project['owner_id'],
+            'for_currency': payment['currency_name']
+        }
+    )
+    if payout_queue:
+        await db.payout_queues.update_one(
+            {'_id': payout_queue['_id']},
+            {'$push': {'queues': payment['payment_id']}}
+        )
+
+
+@payment_router.get('/clear_queue')
+async def clear_queue():
+    await arq_manager.pool.enqueue_job(
+        'task_clear_payout_queue'
+    )
